277b78e3eb0fadda32b22875e75cd24e3edb1a45
cj <carocean.jofers@icloud.com>
2023-05-11 01:25:20 +0800

    1.0.0

--- cj-geochat-ability-oauth2-asc/src/main/java/cj/geochat/ability/oauth2/asc/AbstractResource.java
+++ cj-geochat-ability-oauth2-asc/src/main/java/cj/geochat/ability/oauth2/asc/AbstractResource.java
@@ -1,19 +1,24 @@
 package cj.geochat.ability.oauth2.asc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collection;
-import java.util.Enumeration;
 import java.util.Map;
 
 @Slf4j
 public abstract class AbstractResource {
+    public static final String ENCODING = "ISO-8859-1";
+    public static final String DELIMITER = "\r\n";
+
     protected void doResponse(feign.Response in, HttpServletRequest req, HttpServletResponse out) throws IOException {
         //TODO: 头的拷贝还有问题，一些头可能不合法。postman有时报无效字符,vue根本就无法调用。
+        //vue报：HPE_INVALID_CHUNK_SIZE，如果outputStream.close()则报： HPE_UNEXPECTED_CONTENT_LENGTH
         out.setStatus(in.status());
         Map<String, Collection<String>> srcHeaders = in.headers();
         for (String key : srcHeaders.keySet()) {
@@ -22,15 +27,16 @@ public abstract class AbstractResource {
         }
         InputStream inputStream = in.body().asInputStream();
         OutputStream outputStream = out.getOutputStream();
-        byte[] buffer = new byte[80960];
+        byte[] buffer = new byte[8096];
         while (true) {
             int len = inputStream.read(buffer);
             if (len < 0) {
                 break;
             }
+            //虽然这样能用，但vue要在本地代理中开启header：Connection: 'keep-alive'
             outputStream.write(buffer, 0, len);
+            outputStream.flush();
         }
-        outputStream.flush();
     }
 
     protected byte[] readFully(feign.Response in) throws IOException {
